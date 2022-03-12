95501602040b00f622f1dd03d99e0c0ed2050847
Chen Liang <chen.liang@ie.ibm.com>
2022-03-12 22:48:08 +0000

    Use interface to calculate area of different shapes

---
+++ shape/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+func main() {
+	sq := square{sideLength: 10.0}
+	tr := triangle{height: 3.0, base: 5.0}
+
+	printShape(sq)
+	printShape(tr)
+}
+
+func printShape(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
