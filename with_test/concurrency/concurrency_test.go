21d4e325b3924b8736ef2cf6756ae061ca656ff5
Chen Liang <chen.liang@ie.ibm.com>
2023-05-08 16:37:36 +0100

    Concurrency

---
+++ with_test/concurrency/concurrency_test.go
@@ -0,0 +1,58 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+    results := make(map[string]bool)
+
+    for _, url := range urls {
+        results[url] = wc(url)
+    }
+
+    return results
+}
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds" }
+
+func TestCheckWebsites(t *testing.T) {
+    websites := []string{
+        "http://google.com",
+        "http://blog.gypsydave5.com",
+        "waat://furhurterwe.geds",
+    }
+
+    want := map[string]bool{
+        "http://google.com":    true,
+        "http://blog.gypsydave5.com": true,
+        "waat://furhurterwe.geds": false,
+    }
+
+    got := CheckWebsites(mockWebsiteChecker, websites)
+
+    if !reflect.DeepEqual(want, got) {
+        t.Fatalf("wanted %v, got %v", want, got)
+    }
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+    time.Sleep(20 * time.Millisecond)
+    return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+    urls := make([]string, 100)
+    for i := 0; i < len(urls); i++ {
+        urls[i] = "a url"
+    }
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        CheckWebsites(slowStubWebsiteChecker, urls)
+    }
+}
+
